ab17ced8c13766e65fa80763c8a6cf2a5a3b3a04
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2022-03-25 02:12:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ComecaTerminaCom
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var texto = "esse texto é um teste";
+
+      System.Console.WriteLine(texto.StartsWith("Esse", StringComparison.OrdinalIgnoreCase));
+      System.Console.WriteLine(texto.StartsWith("esse"));
+      System.Console.WriteLine(texto.StartsWith("texto"));
+
+      System.Console.WriteLine(texto.EndsWith("teste"));
+      System.Console.WriteLine(texto.EndsWith("Teste"));
+      System.Console.WriteLine(texto.EndsWith("nunca"));
+    }
+  }
+
+}
